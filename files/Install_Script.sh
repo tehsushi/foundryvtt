7bb3c430faa048dca04cb48acb57bc2966b18572
fithwum <34775371+fithwum@users.noreply.github.com>
2020-06-08 13:04:16 -0700

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -5,20 +5,20 @@
 # Variables.
 echo " "
 echo "Server version is ${F_VTT_VERSION}."
-F_VTT_VERSION=0.6.1
+F_VTT_VERSION=0.6.2
 echo " "
 
 # Main install (alpine).
 # Check if the ini/sh files exist in /FoundryVTT and download/create if needed.
-if [ -e /FoundryVTT/FoundryVTT_minimal_runscript.sh ]
+if [ -e /FoundryVTT/foundryvtt-0.6.2.zip ]
 	then
 		echo "INFO ! FoundryVTT_minimal_runscript.sh found ... will not download."
 	else
 		echo " "
 		echo "WARNING ! FoundryVTT_minimal_runscript.sh not found ... will download new copy."
-			wget --no-cache https://raw.githubusercontent.com/fithwum/files-for-dockers/master/scripts/FoundryVTT_minimal_runscript.sh -O /ts3temp/inifiles/FoundryVTT_minimal_runscript.sh
-			cp /ts3temp/inifiles/FoundryVTT_minimal_runscript.sh /FoundryVTT/
-			rm -fr /ts3temp/FoundryVTT_minimal_runscript.sh
+			wget --no-cache https://nextcloud.fithwum.tech/index.php/s/35GF6g2ro97Etne/download -O /FoundryVTT/foundryvtt-${F_VTT_VERSION}.zip
+			tar -xf /FoundryVTT/foundryvtt-${F_VTT_VERSION}.zip -C /FoundryVTT --strip-components=1
+			rm -fr /FoundryVTT/foundryvtt-${F_VTT_VERSION}.zip
 fi
 
 sleep 1
