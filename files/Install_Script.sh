0c138ff096feaf85b59989aeef5b4077a36b4633
fithwum <34775371+fithwum@users.noreply.github.com>
2020-07-03 21:34:57 -0700

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -3,14 +3,10 @@
 # All rights reserved
 
 # Variables.
-INSTALL_FILES=https://nextcloud.fithwum.tech/index.php/s/35GF6g2ro97Etne/download
 FVTT_VERSION=0.6.2
 
 echo " "
 echo "Server version is ${FVTT_VERSION}."
-echo " "
-
-mkdir -p /foundry/fvtt /foundry/data
 
 sleep 1
 
@@ -21,7 +17,6 @@ chmod 776 -R /foundry
 chmod +x /foundry/
 
 # Run.
-echo " "
 echo "INFO ! Starting FoundryVTT-${FVTT_VERSION}"
 echo " "
 exec node /foundry/fvtt/resources/app/main.js --dataPath=/foundry/data
